103dfc94b439f3c36270f3232ee75ac20931aa24
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-01-30 21:01:56 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,4 +2,11 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
-def
+def batch_badge_creator(attendees)
+  sentences = []
+  attendees.each do |attendee|
+    sentences << "hello, my name is #{attendee}."
+  end
+  sentences
+end
+
